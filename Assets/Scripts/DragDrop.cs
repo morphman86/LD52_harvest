0dbc4026b2bab5ef7c51b1ff2718b08cef0296b0
Chris <ckskola@gmail.com>
2023-01-07 19:34:25 +0000

    Fixed DragDrop mechanics

--- Assets/Scripts/DragDrop.cs
+++ Assets/Scripts/DragDrop.cs
@@ -6,13 +6,12 @@ public class DragDrop : MonoBehaviour
 {
     public bool canMove;
     public bool dragging;
-    Collider2D thisCollider;
+    GameObject draggableObject;
+    public Vector2 startPos;
     void Start()
     {
-        thisCollider = gameObject.GetComponent<Collider2D>();
-        canMove = false;
-        dragging = false;
-
+        this.canMove = false;
+        this.dragging = false;
     }
 
     void Update()
@@ -21,20 +20,49 @@ public class DragDrop : MonoBehaviour
 
         if (Input.GetMouseButtonDown(0))
         {
-            canMove = thisCollider == Physics2D.OverlapPoint(mousePos);
+            try
+            {
+                draggableObject = Physics2D.OverlapPoint(mousePos).gameObject;
+            }
+            catch (System.Exception)
+            {
+                return;
+            }
+            Debug.Log("Clicked on " + draggableObject.name + " with tag" + draggableObject.tag);
+            if(draggableObject.tag == "plant") canMove = true;
+            // canMove = draggableObject.GetComponent<Collider>() == Physics2D.OverlapPoint(mousePos);
             if (canMove)
             {
-                dragging = true;
+                this.startPos = mousePos;
+                Debug.Log("Can move " + draggableObject.name);
+                this.dragging = true;
             }
         }
-        if (dragging)
+        if (this.dragging)
         {
-            this.transform.position = mousePos;
-        }
-        if (Input.GetMouseButtonUp(0))
-        {
-            canMove = false;
-            dragging = false;
+            draggableObject.transform.position = mousePos;
+            if (Input.GetMouseButtonUp(0))
+            {
+                // Check if object is intersecting with an object tagged with "grid"
+                Collider2D[] colliders = Physics2D.OverlapPointAll(mousePos);
+                bool snap = false;
+                foreach (Collider2D collider in colliders)
+                {
+                    if (collider.tag == "grid")
+                    {
+                        Debug.Log("Snapping " + draggableObject.name +" to grid at " + collider.transform.position);
+                        // If it is, snap to grid
+                        draggableObject.transform.position = collider.transform.position;
+                        snap = true;
+                    }
+                }
+                if(!snap){
+                    Debug.Log("Snapping " + draggableObject.name +" to start position at " + startPos);
+                    draggableObject.transform.position = this.startPos;
+                }
+                canMove = false;
+                dragging = false;
+            }
         }
     }
 }
